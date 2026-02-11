1b3d5d3bbf3f835c3b7ba0c80b72d0c03d4be460
Caio Lima <caio.lima1991@yahoo.com.br>
2026-02-11 17:53:03 -0300

    feat: migra para o everforest

--- nvim/.config/nvim/lua/plugins/colortheme.lua
+++ nvim/.config/nvim/lua/plugins/colortheme.lua
@@ -1,8 +1,15 @@
 return {
-  "projekt0n/github-nvim-theme",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme github_dark_default")
-  end,
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = true, -- Can be loaded later if you aren't using it immediately
+  },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
 }
