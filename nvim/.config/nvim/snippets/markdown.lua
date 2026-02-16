a8aed6c24112f2c612f8fbd9a56874d3ce72facf
Caio Lima <caio.lima1991@yahoo.com.br>
2026-02-16 20:42:34 -0300

    feat: cria snippet de notas de referencia e rápida

--- nvim/.config/nvim/snippets/markdown.lua
+++ nvim/.config/nvim/snippets/markdown.lua
@@ -5,14 +5,27 @@ local i = ls.insert_node
 
 local snippets = {
   s("litnote", {
-    t({ "---", "title: " }),
+    t({ "# ", "" }),
+
+    t({ "", "---", "title: " }),
     t({ "", "author: " }),
     t({ "", "source: " }),
     t({ "", "tags: #type/literature " }),
-    t({ "", "---", "", "" }),
+    t({ "", "", "" }),
 
     t("# Summary"),
+    t({ "", "", "# Key Points" }),
     t({ "", "", "# Connections", "" }),
+  }),
+  s("fnote", {
+    t({ "# ", "" }),
+
+    t({ "", "tags: " }),
+    t({ "", "", "" }),
+
+    t("# Raw Thought"),
+
+    t({ "", "", "# Source/Context", "" }),
   }),
 }
 
