61bdb42b6778087c40175531be93f18de3fbf7cd
Caio Lima <caio.lima1991@yahoo.com.br>
2026-02-17 13:16:23 -0300

    feat: remove termux

--- termux/nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.termguicolors = true
-
-require("config.lazy")
-require("init")
--- termux/nvim/.config/nvim/lua/init.lua
+++
@@ -1,9 +0,0 @@
-require("core.options")
-require("core.autocmd")
-
-vim.lsp.config("marksman", {
-	cmd = { "marksman", "server" },
-	filetypes = { "markdown", "markdown.mdx" },
-})
-
-vim.lsp.enable("marksman")
