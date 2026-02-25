9cf922c8b9d9a76a592cbaccc51ed077a4fe68ad
Caio Lima <caio.souza2.prestserv@petrobras.com.br>
2026-02-25 13:52:52 -0300

    feat: retorna register " dentro do Wsl

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,32 @@ vim.opt.termguicolors = true
 vim.g.clipboard = "osc52"
 vim.opt.clipboard = "unnamedplus"
 
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+
+    paste = {
+      ["+"] = function()
+        return {
+          vim.split(vim.fn.getreg('"'), "\n"),
+          vim.fn.getregtype('"'),
+        }
+      end,
+      ["*"] = function()
+        return {
+          vim.split(vim.fn.getreg('"'), "\n"),
+          vim.fn.getregtype('"'),
+        }
+      end,
+    },
+  }
+end
+
 -- Neovim only
 require("config.lazy")
 require("init")
