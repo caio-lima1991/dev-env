e04433511537bdba8cd2b83b6a75d1c1d87de440
Caio <caio.lima1991@yahoo.com.br>
2026-02-25 13:03:47 -0300

    feat: usa a config padrao do OSC52

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,6 @@
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.g.clipboard = "osc52"
-
-vim.g.clipboard = {
-
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-
-  paste = {
-    ["+"] = function()
-      return {}
-    end,
-    ["*"] = function()
-      return {}
-    end,
-  },
-}
-
 vim.opt.clipboard = "unnamedplus"
 
 -- Neovim only
