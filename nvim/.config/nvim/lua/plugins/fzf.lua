e6c754cc4fc8d45ae8d994de6ebf34cc2b6b5b08
Caio Lima <caio.lima1991@yahoo.com.br>
2026-03-06 17:51:05 -0300

    feat: retorna com os built-in fzf keymaps

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -31,15 +31,6 @@ return {
 
       -- FZF Terminal Mappings
       config.defaults.keymap.fzf["ctrl-q"] = "select-all+accept"
-      config.defaults.keymap.fzf["ctrl-u"] = "half-page-up"
-      config.defaults.keymap.fzf["ctrl-d"] = "half-page-down"
-      config.defaults.keymap.fzf["ctrl-x"] = "jump"
-      config.defaults.keymap.fzf["ctrl-f"] = "preview-page-down"
-      config.defaults.keymap.fzf["ctrl-b"] = "preview-page-up"
-
-      -- Builtin UI Mappings
-      config.defaults.keymap.builtin["<c-f>"] = "preview-page-down"
-      config.defaults.keymap.builtin["<c-b>"] = "preview-page-up"
     end,
 
     -- 2. Keymaps
