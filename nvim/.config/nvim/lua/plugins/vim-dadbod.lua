389dcf4d34ad9fba51363e343b175c763aa9c49b
Caio Lima <caio.lima1991@yahoo.com.br>
2026-03-13 19:12:10 -0300

    feat: adiciona toggle do db ui

--- nvim/.config/nvim/lua/plugins/vim-dadbod.lua
+++ nvim/.config/nvim/lua/plugins/vim-dadbod.lua
@@ -10,10 +10,15 @@ return {
       { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
 
+    keys = {
+      { "<leader>D", "<cmd>DBUIToggle<CR>", desc = "Toggle DBUI" },
+    },
+
     init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-      vim.g.db_ui_show_help = 0
-      vim.g.db_ui_win_width = 35
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = { "sql", "mysql", "plsql" },
