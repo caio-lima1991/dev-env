a563f420b7dc9b0987ddf3e389b20a8d375bb4ac
Caio Lima <caio.lima1991@yahoo.com.br>
2026-03-14 18:23:51 -0300

    feat: configura o which key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -7,6 +7,39 @@ return {
       delay = 0,
       spec = {
         { "<leader>u", group = "Undotree", mode = { "n" } },
+        {
+          mode = { "n", "x" },
+          { "<leader>c",  group = "code" },
+          { "<leader>d",  group = "debug" },
+          { "<leader>f",  group = "file/find" },
+          { "<leader>g",  group = "git" },
+          { "<leader>gh", group = "hunks" },
+          { "<leader>q",  group = "quit/session" },
+          { "<leader>s",  group = "search" },
+          { "<leader>x",  group = "dagnostics/quickfix" },
+          { "[",          group = "prev" },
+          { "]",          group = "next" },
+          { "g",          group = "goto" },
+          { "gs",         group = "surround" },
+          { "z",          group = "fold" },
+          { "gx",         desc = "Open with system app" },
+
+          {
+            "<leader>b",
+            group = "buffer",
+            expand = function()
+              return require("which-key.extras").expand.buf()
+            end,
+          },
+          {
+            "<leader>w",
+            group = "windows",
+            proxy = "<c-w>",
+            expand = function()
+              return require("which-key.extras").expand.win()
+            end,
+          },
+        }
       },
     },
   },
