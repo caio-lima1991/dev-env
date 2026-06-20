fe6857824fdc33c5f33b78164ba8a717d714d7fa
caiols <caiols@localhost.localdomain>
2026-06-20 17:47:53 -0300

    refact: remove nvim-treesitter

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,7 @@
 vim.pack.add({
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-mini/mini.icons',
-    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+	"https://github.com/nvim-mini/mini.icons",
+	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
-require('render-markdown').setup({
-    completions = { lsp = { enabled = true } },
+require("render-markdown").setup({
+	completions = { lsp = { enabled = true } },
 })
